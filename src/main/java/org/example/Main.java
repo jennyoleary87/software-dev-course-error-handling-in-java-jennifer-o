19fb02fa8644c1c75dc11a67ee8358113f9a8059
jennyo <jennifer.oleary.10@gmail.com>
2025-06-17 10:20:42 -0500

    complete todos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -28,7 +28,11 @@ public class Main {
      */
     public static double safeDivision(double a, double b) {
         // todo: Handle the unchecked ArithmeticException by returning 0.
-        return divide(a, b);
+       try {
+           return divide(a, b);
+       } catch (ArithmeticException e) {
+           return 0.0;
+       }
     }
 
     /**
@@ -40,10 +44,14 @@ public class Main {
      */
     public static String getSecondLetter(String s, List<String> logMessages) {
         // TODO: Handle the StringIndexOutOfBoundsException by returning an empty string ("")
-        return s.substring(1, 2);
-
-        // TODO: This line needs to happen after the try/catch block, regardless of whether an exception is thrown (put it in a finally block)
-        logMessages.add("Exiting method");
+        try {
+            return s.substring(1, 2);
+        } catch (StringIndexOutOfBoundsException e) {
+            return "";
+        } finally {
+            // TODO: This line needs to happen after the try/catch block, regardless of whether an exception is thrown (put it in a finally block)
+            logMessages.add("Exiting method");
+        }
     }
 
     /**
@@ -54,8 +62,12 @@ public class Main {
      * @return The result of the division
      */
     public static double safeDivideWithCustomException(double a, double b) throws CustomDivideByZeroException {
-        // TODO: handle divide by zero by throwing a CustomDivideByZeroException
-        // TODO: Modify the custom exception to pass a,b to the constructor and store them in the exception (see CustomDivideByZeroException.java)
+        if (b == 0) {
+            // TODO: handle divide by zero by throwing a CustomDivideByZeroException
+            throw new CustomDivideByZeroException(a, b);
+        }
         return divide(a, b);
+        // TODO: Modify the custom exception to pass a,b to the constructor and store them in the exception (see CustomDivideByZeroException.java)
+
     }
 }
